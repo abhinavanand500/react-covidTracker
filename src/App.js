511e55f7463e7f4007a29ed20eed2352bdd707c4
Abhinav Anand <abhinav.anand500@gmail.com>
2020-08-12 01:11:44 +0530

    Working on Cards Completed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import { fetchData } from './api';
+import styles from './App.module.css'
+
+class App extends React.Component {
+    state = {
+        data: {},
+    }
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData })
+    }
+    render() {
+        const { data } = this.state;
+        return (
+            <div className className={styles.container}>
+                <Cards data={data} />
+                <Chart />
+                <CountryPicker />
+            </div>
+        );
+    }
+}
+
+export default App;
