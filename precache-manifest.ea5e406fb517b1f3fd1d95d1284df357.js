2f1fc5ab3ce4d9bbf10e194106f85594a50290dc
Abhinav Anand <abhinav.anand500@gmail.com>
2020-08-12 15:50:18 +0530

    Updates

---
+++ precache-manifest.ea5e406fb517b1f3fd1d95d1284df357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105ee9ece032d6d8994035e1cf50e8c3",
+    "url": "/react-covidTracker/index.html"
+  },
+  {
+    "revision": "6e70b3c8384ac4d8b285",
+    "url": "/react-covidTracker/static/css/main.e3994992.chunk.css"
+  },
+  {
+    "revision": "503b975b04e669e7aa16",
+    "url": "/react-covidTracker/static/js/2.11f4ce2a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covidTracker/static/js/2.11f4ce2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e70b3c8384ac4d8b285",
+    "url": "/react-covidTracker/static/js/main.020074ac.chunk.js"
+  },
+  {
+    "revision": "e50fe3140157ce06b713",
+    "url": "/react-covidTracker/static/js/runtime-main.31c6809e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covidTracker/static/media/logo.d7265326.png"
+  }
+]);
